ee288822c1761b3ec0ebf1db6b1214aabf9721d4
Bruce <contact@estimatebodyfat.com>
2022-06-19 14:31:10 -0400

    initial work

--- app.js
+++ app.js
@@ -1,48 +1,31 @@
-//jshint esversion:6
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const date = require(__dirname + "/date.js")
+const mongoose = require('monogoose')
+const _ = require('lodash')
+const PORT = 3000
+require('dotenv').config()
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
 
-const app = express();
 
-app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect(process.env.DB_STRING, {dbName: 'todolistDB'}, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});  
+ 
 
-app.get("/", function(req, res) {
-
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
-
-});
-
-app.post("/", function(req, res){
-
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
-});
-
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
-
+ 
 app.get("/about", function(req, res){
-  res.render("about");
+  res.render("about.ejs");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || PORT, function() {
+  console.log(`Server is running on PORT ${PORT}`);
 });
